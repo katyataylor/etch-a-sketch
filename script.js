85009e4da98cff8882c56312cada23a1ab944d7c
kat.taylor <sofia.katya.taylor@gmail.com>
2026-07-19 11:54:41 -0500

    Rewrite HTML & JS following assignment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-window.addEventListener(`DOMContentLoaded`, () => {
+// Select the container div
+const container = document.querySelector('#container');
 
-    let startX = 0;
-    let startY = 0;
+// Run a loop 256 times to create a 16x16 layout
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement('div');
+    
+    // Add the CSS class for styling and dimensional math
+    square.classList.add('grid-square');
+    
+    // Add a quick hover effect to test if grid functions
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'rgba(0, 0, 0, 0.05)';
+        square.style.boxShadow = '0 4px 8px rgba (0, 0, 0, 0.1)';
+    });
 
-});
+    square.addEventListener('mouseleave', () => {
+        square.style.backgroundColor = '';
+    });
+
+    // Append the newly created div inside container
+    container.appendChild(square);
+}
